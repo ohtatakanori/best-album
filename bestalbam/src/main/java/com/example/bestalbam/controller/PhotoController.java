e0983684e82f188cc0b96b09e820a0a652b67c48
ohtatakanori <166459652+ohtatakanori@users.noreply.github.com>
2024-07-16 22:51:02 +0900

    Update PhotoController.java

--- bestalbam/src/main/java/com/example/bestalbam/controller/PhotoController.java
+++ bestalbam/src/main/java/com/example/bestalbam/controller/PhotoController.java
@@ -1,53 +1,53 @@
-// package com.example.bestalbam.controller;
+package com.example.bestalbam.controller;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.security.core.annotation.AuthenticationPrincipal;
-// import org.springframework.security.core.userdetails.UserDetails;
-// import org.springframework.stereotype.Controller;
-// import org.springframework.ui.Model;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RequestParam;
-// import org.springframework.web.multipart.MultipartFile;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
-// import com.example.bestalbam.model.Photo;
-// import com.example.bestalbam.service.PhotoService;
+import com.example.bestalbam.model.Photo;
+import com.example.bestalbam.service.PhotoService;
 
-// @Controller
-// @RequestMapping("/photos")
-// public class PhotoController {
-//     @Autowired
-//     private PhotoService photoService;
+@Controller
+@RequestMapping("/photos")
+public class PhotoController {
+    @Autowired
+    private PhotoService photoService;
 
-//     @GetMapping("/add")
-//     public String addPhoto(@AuthenticationPrincipal UserDetails currentUser, Model model) {
-//         model.addAttribute("photo", new Photo());
-//         return "photos/photo_add";
-//     }
+    @GetMapping("/add")
+    public String addPhoto(@AuthenticationPrincipal UserDetails currentUser, Model model) {
+        model.addAttribute("photo", new Photo());
+        return "photos/photo_add";
+    }
 
-//     @PostMapping("/add")
-//     public String add(Photo photo) {
-//         photoService.save(photo);
-//         return "redirect:/photos";
-//     }
+    @PostMapping("/add")
+    public String add(Photo photo) {
+        photoService.save(photo);
+        return "redirect:/photos";
+    }
     
-//     @GetMapping("add/pictures")
-//     public String pictureForm(@PathVariable("id") Long id, Model model) {
-//         Photo photo = photoService.findPhotoById(id);
-//         model.addAttribute("photo", photo);
-//         return "/admin/course-image-add";
-//     }
+    @GetMapping("add/pictures")
+    public String pictureForm(@PathVariable("id") Long id, Model model) {
+        Photo photo = photoService.findPhotoById(id);
+        model.addAttribute("photo", photo);
+        return "/admin/course-image-add";
+    }
 
-//     @PostMapping("add/pictures")
-//     public String pictureUp(@RequestParam("file") MultipartFile file, @RequestParam("id") Long id) {
-//         String originalFilename = file.getOriginalFilename();
-//         String fileExtension = originalFilename != null ? originalFilename.substring(originalFilename.lastIndexOf(".") + 1).toLowerCase() : "";
-//         if (!fileExtension.equals("png")) {
-//             return "redirect:/admin/courses";
-//         }
-//         photoService.pictureUp(file, id);
-//         return "redirect:/admin/courses";
-//     }
-// }
+    @PostMapping("add/pictures")
+    public String pictureUp(@RequestParam("file") MultipartFile file, @RequestParam("id") Long id) {
+        String originalFilename = file.getOriginalFilename();
+        String fileExtension = originalFilename != null ? originalFilename.substring(originalFilename.lastIndexOf(".") + 1).toLowerCase() : "";
+        if (!fileExtension.equals("png")) {
+            return "redirect:/admin/courses";
+        }
+        photoService.pictureUp(file, id);
+        return "redirect:/admin/courses";
+    }
+}
